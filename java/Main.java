1c4cba0d71d5c1457e84e53311f7e77baae5a3fa
Anastasios Antoniadis <a.antoniadis1989@gmail.com>
2014-10-09 13:44:49 +0300

    java part done

--- java/Main.java
+++ java/Main.java
@@ -9,41 +9,30 @@ public class Main {
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-                Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
-                String line;
-                ArrayList<int[]> output = new ArrayList<>();
+            Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+            String line;
+            ArrayList<int[]> output = new ArrayList<>();
 
-                while ((line = br.readLine()) != null) {
-                    String[] parts = line.split("\\|");
-                    int part1 = Integer.parseInt(parts[0]), part2 = Integer.parseInt(parts[1]);
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                int part1 = Integer.parseInt(parts[0]), part2 = Integer.parseInt(parts[1]);
 
-                    if (map.containsKey(part1)) {
-                        Integer seqNumber;
-                        Map tempMap = map.get(part1);
+                if (!map.containsKey(part1))
+                    map.put(part1, new HashMap<>());
 
-                        if ((seqNumber = (map.get(part1).get(part2))) != null) {
-                            int[] array = {part1, part2, seqNumber, 0};
-                            output.add(array);
-                        }
-                        else {
-                            Map temp = map.get(part1);
-                            seqNumber = temp.keySet().size()+1;
-                            temp.put(part2, seqNumber);
-                            int[] array = {part1, part2, seqNumber, 0};
-                            output.add(array);
-                        }
-                    }
-                    else {
-                        Map seqMap = new HashMap<>();
-                        seqMap.put(part2, 1);
-                        map.put(part1, seqMap);
-                        int[] array = {part1, part2, 1, 0};
-                        output.add(array);
-                    }
+                Integer seqNumber;
+                if ((seqNumber = (map.get(part1).get(part2))) == null){
+                    Map<Integer, Integer> temp = map.get(part1);
+                    seqNumber = temp.keySet().size()+1;
+                    temp.put(part2, seqNumber);
                 }
-                for (int[] array: output)
-                    System.out.println(array[0] + "|" + array[1] + "[" + array[2] + " of " + map.get(array[0]).keySet().size() + "]");
+
+                int[] array = {part1, part2, seqNumber};
+                output.add(array);
             }
+            for (int[] array: output)
+                System.out.println(array[0] + "|" + array[1] + "[" + array[2] + " of " + map.get(array[0]).keySet().size() + "]");
+        }
         catch (IOException ex) {
             System.out.println(ex.toString());
             System.exit(-1);
